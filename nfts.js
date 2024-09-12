fa915761cbc2b9da1c28442c9467f666b8ddec3a
anshu-jangra-01 <vsanshu364@gmail.com>
2024-09-12 22:52:18 +0530

    Create nfts.js

---
+++ nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+    {
+        name: '0',
+        description: 'Potrait of princess',
+        image: 'https://olive-intellectual-louse-350.mypinata.cloud/ipfs/QmUQdobo6LikRUykVP2tyTq8nYbTWmnCsqoZRnjsnsjUnw'
+     },
+    {
+        name: '1',
+        description: 'Potrait of princess',
+        image: 'https://olive-intellectual-louse-350.mypinata.cloud/ipfs/QmWbiEL5dY5pLP9jvFcGBpPCfSFrJwh5gcQ7Dkh2rRvAcR'
+    },//change
+    {
+        name: '2',
+        description: 'Potrait of princess',
+        image: 'https://olive-intellectual-louse-350.mypinata.cloud/ipfs/QmRe84HAgpmeqN2FHEFfV1JcrZu7ZVi9sEEen5evAdYCRq'
+    },
+    {
+        name: '3',
+        description: 'Potrait of princess',
+        image: 'https://olive-intellectual-louse-350.mypinata.cloud/ipfs/QmPpYMPorTuUc6TGA4fNtaNj989fF5EfKZWbMyqwiJjJwD'
+    },
+    {
+        name: '4',
+        description: 'Potrait of princess',
+        image: 'https://olive-intellectual-louse-350.mypinata.cloud/ipfs/Qmez3r4ufAAGKz2yaAnXBLX4gDaS2g28pCU89ixhLVWiSN'
+    },    
+];
+
+module.exports = nfts;
